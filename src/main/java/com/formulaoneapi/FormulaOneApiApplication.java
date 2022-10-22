6595d5338cad519cdf40c3a89a2a0cd16ab8f06c
Damian Lukasiewicz <damian.lukasiewicz99@gmail.com>
2022-10-22 12:23:50 +0200

    spring initializr

---
+++ src/main/java/com/formulaoneapi/FormulaOneApiApplication.java
@@ -0,0 +1,13 @@
+package com.formulaoneapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormulaOneApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FormulaOneApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/formulaoneapi/FormulaOneApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.formulaoneapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FormulaOneApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
