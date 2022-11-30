c7a4cddaa89bd893e52ebacac3165fd96672e54e
Łukasz Michalski <76202883+lukaszmichalskii@users.noreply.github.com>
2022-11-30 19:07:22 +0100

    Accident service (#15)
    * Possible accidents to report
    
    * Accident data endpoint handling
    
    * Fix 'Internal Server Error' while trying to delete non existing part
    
    * Accident POST/PUT requests fails if 'description' exceeds 1000 char limit
    
    * Proper POST/PUT requests handling, Accident model constraints
    
    * Does not allow POST with empty Accident name field in request body
    
    Co-authored-by: Lukasz Michalski <lukasz.michalski@nokia.com>

--- src/main/java/com/formulaoneapi/model/Accident.java
+++ src/main/java/com/formulaoneapi/model/Accident.java
@@ -1,17 +1,27 @@
 package com.formulaoneapi.model;
 
+import com.formulaoneapi.validation.groups.IdValidation;
 import lombok.Data;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 @Entity
 @Data
 public class Accident {
     @Id
+    @NotBlank(groups = {IdValidation.class}, message = "Name cannot be blank" )
+    @Size(
+            max = 255,
+            message = "Accident name must not exceed 255 characters",
+            groups = {IdValidation.class} )
     private String name;
 
-    @Column( length = 1000 )
+    @Size(max = 1000, message = "Description is textual definition of accident and cannot exceeded 1000 char limit.")
+    @Column(length = 1000)
+    @NotBlank(message = "Description cannot be blank.")
     private String description;
 }
---
+++ src/main/java/com/formulaoneapi/service/AccidentService.java
@@ -0,0 +1,57 @@
+package com.formulaoneapi.service;
+
+import com.formulaoneapi.exception.ElementAlreadyExistsException;
+import com.formulaoneapi.model.Accident;
+import com.formulaoneapi.repository.AccidentRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.NoSuchElementException;
+
+@Service
+@RequiredArgsConstructor
+public class AccidentService {
+
+    private final AccidentRepository accidentRepository;
+
+    public Iterable<Accident> getAll() { return accidentRepository.findAll(); }
+
+    public Accident get(String name) {
+        return accidentRepository
+                .findById(name)
+                .orElseThrow(
+                        () -> new NoSuchElementException(String.format("Accident type '%s' not found", name))
+                );
+    }
+
+    @Transactional
+    public void remove(String name) {
+        assertAccidentDoesNotExist(name);
+        accidentRepository.deleteById(name);
+    }
+
+    @Transactional
+    public Accident update(Accident accident) {
+        assertAccidentDoesNotExist(accident.getName());
+        return accidentRepository.save(accident);
+    }
+
+    public Accident save(Accident accident) {
+        assertAccidentExists(accident.getName());
+        return accidentRepository.save(accident);
+    }
+
+    private void assertAccidentDoesNotExist(String name) {
+        if (!accidentRepository.existsById(name)) {
+            throw new NoSuchElementException(String.format("Accident type '%s' not found", name));
+        }
+    }
+
+    private void assertAccidentExists(String name) {
+        if (accidentRepository.existsById(name)) {
+            throw new ElementAlreadyExistsException(String.format("Accident type '%s' already exists", name));
+        }
+    }
+
+}
